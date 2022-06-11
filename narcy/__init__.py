8f2b0598fb525145b4ba869c38d54395bfa69ec9
pdpetrova <43388379+pdpetrova@users.noreply.github.com>
2022-06-12 00:02:40 +0300

    Update __init__.py

--- narcy/__init__.py
+++ narcy/__init__.py
@@ -1,5 +1,5 @@
 from .nlp import spacy_ext
-from .nlp.utils import document_factory
+from .nlp.utils import document_factory, get_compound_verb, get_compound_noun
 from .processors import doc_to_relations_df, doc_to_svos_df, doc_to_tokens_df
 
 __author__ = 'Szymon Talaga'
