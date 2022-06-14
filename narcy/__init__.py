45f7033248adcfa78efa06e0dc8ce55408734acb
pdpetrova <43388379+pdpetrova@users.noreply.github.com>
2022-06-15 00:47:59 +0300

    Update __init__.py

--- narcy/__init__.py
+++ narcy/__init__.py
@@ -1,6 +1,6 @@
 from .nlp import spacy_ext
 from .nlp.utils import document_factory, get_compound_verb, get_compound_noun, detect_tense
-from .processors import doc_to_relations_df, doc_to_svos_df, doc_to_tokens_df, doc_to_locs_df
+from .processors import doc_to_relations_df, doc_to_svos_df, doc_to_tokens_df, get_all_relations
 
 __author__ = 'Szymon Talaga'
 __email__ = 'stalaga@protonmail.com'
